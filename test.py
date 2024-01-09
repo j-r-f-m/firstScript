2cecfb924ab367e45c24d86f09a73d4e69c94916
jrfm <moesch.jonas@protonmail.com>
2024-01-09 19:38:15 +0100

    add comments to script

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
 
-cl = FilteredElementCollector(doc)
-cl.OfCategory(BuiltInCategory.OST_Walls)
-cl.WhereElementIsNotElementType()
+# # Pass the current document you want to search to the collector
+# cl = FilteredElementCollector(doc)
+# # Filter the collector to only include walls
+# cl.OfCategory(BuiltInCategory.OST_Walls)
+# # Filter the collector to only include elements that are not types
+# cl.WhereElementIsNotElementType()
 
-for el in cl:
-    print(el.Name)  # prints the name of each wall in the projectss
+
+wall_collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType()
+
+for wall in wall_collector:
+    print(wall)    # Prints the element ID of each wall in the project
